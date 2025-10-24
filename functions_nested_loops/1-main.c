426f7aae596224bb15a4bcb75d6abd01963b6839
sam99-web <keishsam99@gmail.com>
2025-10-24 14:01:33 +0000

    task1

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet(); /* Maintenant, il connaît la fonction *
+			     /
+	return (0);
+}
+
