0930bfde6641a07d81974ede3dcd3779f2a4392b
sam99-web <keishsam99@gmail.com>
2025-10-25 22:35:21 +0000

    task6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the integer to check
+ *
+ * Return: the absolute value of n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,10 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
+int _abs(int n);
 
 #endif /* MAIN_H */
+
 
