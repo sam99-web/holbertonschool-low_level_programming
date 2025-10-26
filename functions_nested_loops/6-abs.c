4eb4c9a69a2eb4c6a7a44be02ceba9432cb12a95
root <keishsam99@gmail.com>
2025-10-26 12:03:13 +0000

    task6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,14 +2,14 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: the integer to check
+ * @n: the integer to compute the absolute value for
  *
- * Return: the absolute value of n
+ * Return: absolute value of n
  */
 int _abs(int n)
 {
 	if (n < 0)
-		n = -n;
+		return (-n);
 	return (n);
 }
 
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_alphabet_x10(void);
+int _abs(int);
 
 #endif /* MAIN_H */
 
