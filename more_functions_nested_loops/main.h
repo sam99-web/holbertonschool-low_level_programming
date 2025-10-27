256e16a29b55a80f0e80694718f1c00c18690609
sam99-web <keishsam99@gmail.com>
2025-10-27 12:19:09 +0000

    task0

---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to check
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c);
+
+#endif /* MAIN_H */
+
---
+++ more_functions_nested_loops/man.h
@@ -0,0 +1,5 @@
+#ifndef_MAIN_H
+#define_MAIN_H
+
+int _isdigit(int c);
+#endif 
