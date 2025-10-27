7b8f0e2c4c7afb16812812a5615dcd33f92b7d98
sam99-web <keishsam99@gmail.com>
2025-10-27 13:33:01 +0000

    task5

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * print_most_numbers - prints_most_numbers from 0 to 9 followed by a new line
+ * more_numbers - more_numbers from 0 to 14 followed by a new line
  */
-void print_most_numbers(void);
+void more_numbers(void);
 
 /**
  * _putchar - writes the character c to stdout
