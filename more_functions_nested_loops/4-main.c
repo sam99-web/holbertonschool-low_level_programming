8c5302371f214df950e82085b83021c3fa746d28
sam99-web <keishsam99@gmail.com>
2025-10-27 13:17:56 +0000

    task4

---
+++ more_functions_nested_loops/4-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the print_most_numbers function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	print_most_numbers();
+	return (0);
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * print_numbers - prints numbers from 0 to 9 followed by a new line
+ * print_most_numbers - prints_most_numbers from 0 to 9 followed by a new line
  */
-void print_numbers(void);
+void print_most_numbers(void);
 
 /**
  * _putchar - writes the character c to stdout
