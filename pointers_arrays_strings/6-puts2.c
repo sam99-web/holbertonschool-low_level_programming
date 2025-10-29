2b10a1af937a95a7b95d7946163dbe3263e3cc8e
sam99-web <keishsam99@gmail.com>
2025-10-29 10:26:24 +0000

    task6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * puts2 - affiche un caractère sur deux d'une chaîne
- * @str: la chaîne de caractères à afficher
+ * puts2 - prints one character out of two of a string
+ * @str: the string to be processed
  *
- * Retour: rien
+ * Return: nothing
  */
 void puts2(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i] != '\0')
-    {
-        if (i % 2 == 0)
-            _putchar(str[i]);
-        i++;
-    }
-    _putchar('\n');
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
