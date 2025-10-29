2d184cebac8e02bcfa2a74b9314c97e7bec3ca1c
sam99-web <keishsam99@gmail.com>
2025-10-29 15:01:42 +0000

    task4

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,15 +1,21 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code
+ * main - check the code for reverse_array function
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *str;
+	int n;
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    print_rev(str);
-    return (0);
+	n = sizeof(a) / sizeof(int);
+	print_array(a, n);
+	printf("\n");
+	reverse_array(a, n);
+	print_array(a, n);
+	printf("\n");
+	return (0);
 }
