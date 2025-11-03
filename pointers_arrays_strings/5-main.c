bd71d6f34ae08e1ab1acf756230fdb5f2146fade
sam99-web <keishsam99@gmail.com>
2025-11-03 14:50:24 +0000

    task5

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - check the code for string_toupper function
+ * main - check the code for _strstr function
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char str[] = "Holberton School!\nThis is a test String 123.";
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
 
-	printf("%s", str);
-	string_toupper(str);
-	printf("%s", str);
-
-	return (0);
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
 }
