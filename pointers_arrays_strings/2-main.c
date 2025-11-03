4027b5f55b2eb7cf399d5312b59e40f07ea26f61
sam99-web <keishsam99@gmail.com>
2025-11-03 14:15:14 +0000

    task1

--- pointers_arrays_strings/2-main.c
+++ pointers_arrays_strings/2-main.c
@@ -2,42 +2,20 @@
 #include <stdio.h>
 
 /**
- * main - check the code for _strncpy function
+ * main - check the code for _strchr function
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char s1[98];
-	char *ptr;
-	int i;
+	char *s = "hello";
+	char *f;
 
-	for (i = 0; i < 98 - 1; i++)
-	{
-		s1[i] = '*';
-	}
-	s1[i] = '\0';
-	printf("%s\n", s1);
-	ptr = _strncpy(s1, "First, solve the problem. Then, write the code.\n", 5);
-	printf("%s\n", s1);
-	printf("%s\n", ptr);
-	ptr = _strncpy(s1, "First, solve the problem. Then, write the code.\n", 90);
-	printf("%s", s1);
-	printf("%s", ptr);
+	f = _strchr(s, 'l');
 
-	for (i = 0; i < 98; i++)
+	if (f != NULL)
 	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", s1[i]);
+		printf("%s\n", f);
 	}
-	printf("\n");
 	return (0);
 }
-
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,7 +6,8 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src); 
+/* char *_strcat(char *dest, char *src); */
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -15,5 +16,8 @@ void print_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif 
