3eda7e6bd256fbd10448bb2581c297ebfa5be50d
sam99-web <keishsam99@gmail.com>
2025-11-05 20:24:46 +0000

    task6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -14,3 +14,15 @@ int check_prime(int n, int i)
         return (0);    /* Pas premier */
     return (check_prime(n, i + 1)); /* Sinon, on essaie avec le suivant */
 }
+
+/**
+ * is_prime_number - Détermine si un entier est premier
+ * @n: le nombre à tester
+ * Return: 1 si premier, 0 sinon
+ */
+int is_prime_number(int n)
+{
+    if (n <= 1)        /* 0, 1, négatifs → pas premiers */
+        return (0);
+    return (check_prime(n, 2));  /* Appel récursif à partir de 2 */
+}
