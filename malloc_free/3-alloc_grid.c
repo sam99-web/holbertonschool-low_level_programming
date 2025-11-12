cdf04d0880d3f9b350e88d67ee26cd3b9510b09f
sam99-web <keishsam99@gmail.com>
2025-11-12 15:53:02 +0000

    task3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,44 +1,40 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - crée une grille 2D d'entiers et l'initialise à 0
- * @width: largeur de la grille (nombre de colonnes)
- * @height: hauteur de la grille (nombre de lignes)
+ * alloc_grid - returns a pointer to a 2D array of integers
+ * @width: number of columns
+ * @height: number of rows
  *
- * Return: pointeur vers la grille 2D, ou NULL en cas d'erreur
- *         (width <= 0, height <= 0, ou échec malloc)
+ * Return: pointer to the 2D array, or NULL on failure
  */
 int **alloc_grid(int width, int height)
 {
-    int **grid;
-    int i, j;
+	int **grid;
+	int i, j;
 
-    /* Vérification des dimensions */
-    if (width <= 0 || height <= 0)
-        return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-    /* Allocation du tableau de pointeurs (lignes) */
-    grid = malloc(sizeof(int *) * height);
-    if (grid == NULL)
-        return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
 
-    /* Allocation de chaque ligne et initialisation à 0 */
-    for (i = 0; i < height; i++)
-    {
-        grid[i] = malloc(sizeof(int) * width);
-        if (grid[i] == NULL)
-        {
-            /* Libérer les lignes déjà allouées en cas d'échec */
-            for (j = 0; j < i; j++)
-                free(grid[j]);
-            free(grid);
-            return (NULL);
-        }
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			while (i >= 0)
+			{
+				free(grid[i]);
+				i--;
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
 
-        /* Initialiser la ligne à 0 */
-        for (j = 0; j < width; j++)
-            grid[i][j] = 0;
-    }
-
-    return (grid);
+	return (grid);
 }
