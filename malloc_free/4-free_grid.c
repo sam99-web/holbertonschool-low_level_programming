acfccab5fbb2ad283b92937d2065e03ca618a87e
sam99-web <keishsam99@gmail.com>
2025-11-12 15:57:51 +0000

    task4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,24 +1,21 @@
 #include <stdlib.h>
 
 /**
- * free_grid - libère une grille 2D d'entiers créée par alloc_grid
- * @grid: pointeur vers la grille à libérer
- * @height: nombre de lignes de la grille
+ * free_grid - frees a 2D grid previously created by alloc_grid
+ * @grid: pointer to the 2D grid
+ * @height: number of rows of the grid
+ *
+ * Return: nothing
  */
 void free_grid(int **grid, int height)
 {
-    int i;
+	int i;
 
-    if (grid == NULL)
-        return;
+	if (grid == NULL || height <= 0)
+		return;
 
-    /* Libérer chaque ligne */
-    for (i = 0; i < height; i++)
-    {
-        if (grid[i] != NULL)
-            free(grid[i]);
-    }
+	for (i = 0; i < height; i++)
+		free(grid[i]);
 
-    /* Libérer le tableau de pointeurs */
-    free(grid);
+	free(grid);
 }
