49134ce55764207339d983e8cb930e62fbdd949b
sam99-web <keishsam99@gmail.com>
2025-11-12 15:48:49 +0000

    task2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,47 +1,42 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatène deux chaînes dans un nouvel espace mémoire.
- * @s1: première chaîne (peut être NULL).
- * @s2: seconde chaîne (peut être NULL).
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
  *
- * Return: pointeur vers la nouvelle chaîne concaténée,
- *         ou NULL si allocation échoue.
+ * Return: pointer to newly allocated memory containing s1 + s2,
+ * or NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-    char *concat;
-    unsigned int i, j;
-    unsigned int len1 = 0, len2 = 0;
+	char *concat;
+	int i, j, len1 = 0, len2 = 0;
 
-    /* Traiter les chaînes NULL comme des chaînes vides */
-    if (s1 != NULL)
-    {
-        for (len1 = 0; s1[len1] != '\0'; len1++)
-            ;
-    }
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    if (s2 != NULL)
-    {
-        for (len2 = 0; s2[len2] != '\0'; len2++)
-            ;
-    }
+	/* Calculate lengths of both strings */
+	while (s1[len1] != '\0')
+		len1++;
+	while (s2[len2] != '\0')
+		len2++;
 
-    /* Allouer la mémoire pour la nouvelle chaîne (+1 pour '\0') */
-    concat = malloc(sizeof(char) * (len1 + len2 + 1));
-    if (concat == NULL)
-        return (NULL);
+	concat = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (concat == NULL)
+		return (NULL);
 
-    /* Copier s1 dans concat */
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+	/* Copy s1 into concat */
+	for (i = 0; i < len1; i++)
+		concat[i] = s1[i];
 
-    /* Copier s2 dans concat après s1 */
-    for (j = 0; j < len2; j++)
-        concat[i + j] = s2[j];
+	/* Copy s2 after s1 */
+	for (j = 0; j < len2; j++)
+		concat[i + j] = s2[j];
 
-    /* Terminer la chaîne */
-    concat[len1 + len2] = '\0';
+	concat[i + j] = '\0';
 
-    return (concat);
+	return (concat);
 }
