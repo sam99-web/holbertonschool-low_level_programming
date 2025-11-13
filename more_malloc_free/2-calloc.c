bd120c766ed83e5ae168f50b00deb483470994a3
sam99-web <keishsam99@gmail.com>
2025-11-13 11:29:15 +0000

    task2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,32 +1,32 @@
 #include "main.h"
 
 /**
- * _calloc - allocates memory for an array, initialized to zero
+ * _calloc - allocates memory for an array and initializes it to zero
  * @nmemb: number of elements
  * @size: size in bytes of each element
  *
  * Description: Allocates memory for an array of nmemb elements of size bytes.
- * The allocated memory is initialized to zero. If nmemb or size is 0,
- * or if malloc fails, the function returns NULL.
+ * Each byte of the allocated memory is initialized to zero. Returns NULL if
+ * nmemb or size is 0, or if malloc fails.
  *
- * Return: Pointer to allocated memory (zeroed) or NULL on failure.
+ * Return: pointer to allocated memory, or NULL on failure
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *ptr;
-    unsigned int total, i;
+	char *ptr;
+	unsigned int i, total;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    total = nmemb * size;
+	total = nmemb * size;
 
-    ptr = malloc(total);
-    if (ptr == NULL)
-        return (NULL);
+	ptr = malloc(total);
+	if (ptr == NULL)
+		return (NULL);
 
-    for (i = 0; i < total; i++)
-        ptr[i] = 0;
+	for (i = 0; i < total; i++)
+		ptr[i] = 0;
 
-    return ((void *)ptr);
+	return ((void *)ptr);
 }
