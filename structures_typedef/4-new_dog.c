c190d027e24543f146e385949e8a9efd6e2bb0b9
sam99-web <keishsam99@gmail.com>
2025-11-14 10:55:41 +0000

    task4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,81 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - retourne la longueur d'une chaîne
+ * @s: chaîne à mesurer
+ *
+ * Return: longueur de la chaîne
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strcpy - copie une chaîne dans une autre
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointeur vers dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
+
+/**
+ * new_dog - crée un nouveau chien
+ * @name: nom du chien
+ * @age: âge du chien
+ * @owner: propriétaire du chien
+ *
+ * Return: pointeur vers la nouvelle structure dog_t, ou NULL si échec
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len_name, len_owner;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+
+	len_name = _strlen(name);
+	len_owner = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len_name + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (len_owner + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
