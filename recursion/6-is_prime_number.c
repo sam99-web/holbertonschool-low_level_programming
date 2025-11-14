007d1b9c4958eb8dfc2701ade092bcb86904774f
sam99-web <keishsam99@gmail.com>
2025-11-14 11:50:06 +0000

    task6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,28 +1,33 @@
 #include "main.h"
 
 /**
- * check_prime - Vérifie récursivement si n est divisible par i
- * @n: le nombre à tester
- * @i: le diviseur potentiel
- * Return: 1 si premier, 0 sinon
+ * check_prime - checks recursively if n is divisible by i
+ * @n: number to check
+ * @i: current divisor
+ *
+ * Return: 1 if prime, 0 otherwise
  */
 int check_prime(int n, int i)
 {
-    if (i * i > n)     /* Si on a dépassé la racine carrée */
-        return (1);    /* Aucun diviseur trouvé → premier */
-    if (n % i == 0)    /* Si n est divisible par i */
-        return (0);    /* Pas premier */
-    return (check_prime(n, i + 1)); /* Sinon, on essaie avec le suivant */
+	if (i * i > n)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	return (check_prime(n, i + 1));
 }
 
 /**
- * is_prime_number - Détermine si un entier est premier
- * @n: le nombre à tester
- * Return: 1 si premier, 0 sinon
+ * is_prime_number - determines if a number is a prime number
+ * @n: number to evaluate
+ *
+ * Return: 1 if prime, 0 otherwise
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)        /* 0, 1, négatifs → pas premiers */
-        return (0);
-    return (check_prime(n, 2));  /* Appel récursif à partir de 2 */
+	if (n <= 1)
+		return (0);
+
+	return (check_prime(n, 2));
 }
