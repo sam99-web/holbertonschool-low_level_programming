dc513085f9da4b5a7c333338787a10ac694f54ec
sam99-web <keishsam99@gmail.com>
2025-11-14 12:08:11 +0000

    atsk4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -3,24 +3,26 @@
 
 /**
  * _strpbrk - searches a string for any of a set of bytes
- * @s: the string to search
- * @accept: the set of bytes to match
+ * @s: string to search
+ * @accept: characters to match
  *
- * Return: pointer to the byte in s that matches one of the bytes in accept,
- * or NULL if no such byte is found
+ * Return: pointer to the first match in s,
+ *         or NULL if no character matches
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-                return (s + i);
-        }
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
 
-    return (NULL);
+	return (NULL);
 }
