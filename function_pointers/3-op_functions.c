70b9dc976541f0ef762de778687f4a17a9234f6c
sam99-web <keishsam99@gmail.com>
2025-11-19 12:38:09 +0000

    task3

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns sum of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns difference of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns product of a and b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns result of division of a by b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - returns remainder of division of a by b
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
